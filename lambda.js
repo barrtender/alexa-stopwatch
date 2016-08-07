68ac4c9dba71173f5c289fadf4de821fc26500cd
Eric Barr <barrtender@gmail.com>
2016-08-06 21:06:39 -0700

    added database connection. testing on dev console works

--- lambda.js
+++ lambda.js
@@ -4,6 +4,10 @@
 
 'use strict';
 
+
+var AWS = require("aws-sdk");
+var dynamodb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
+
 /**
  * When editing your questions pay attention to your punctuation. Make sure you use question marks or periods.
  * Make sure the first answer is the correct one. Set at least 4 answers, any extras will be shuffled in.
@@ -79,12 +83,13 @@ function onSessionStarted(sessionStartedRequest, session) {
 
 /**
  * Called when the user invokes the skill without specifying what they want.
+ * 
  */
 function onLaunch(launchRequest, session, callback) {
     console.log("onLaunch requestId=" + launchRequest.requestId
         + ", sessionId=" + session.sessionId);
 
-    getWelcomeResponse(callback);
+    getWelcomeResponse(session, callback);
 }
 
 /**
@@ -113,13 +118,13 @@ function onIntent(intentRequest, session, callback) {
     // Get help
     switch(intentName){
         case "AMAZON.StartOverIntent":
-            getWelcomeResponse(callback);
+            getWelcomeResponse(session, callback);
             break;
         case "AMAZON.StopIntent":
             handleFinishSessionRequest(intent, session, callback);
             break;
         case "CheckTimeIntent":
-            handleCheckRequest(intent, session, callback);
+            handleCheckRequest(session, callback);
             break;
         case "AMAZON.HelpIntent":
             handleGetHelpRequest(intent, session, callback);
@@ -127,31 +132,6 @@ function onIntent(intentRequest, session, callback) {
         default:
             throw "Invalid intent";
     }
-/*
-    // dispatch custom intents to handlers here
-    if ("AnswerIntent" === intentName) {
-        handleAnswerRequest(intent, session, callback);
-    } else if ("AnswerOnlyIntent" === intentName) {
-        handleAnswerRequest(intent, session, callback);
-    } else if ("DontKnowIntent" === intentName) {
-        handleAnswerRequest(intent, session, callback);
-    } else if ("AMAZON.YesIntent" === intentName) {
-        handleAnswerRequest(intent, session, callback);
-    } else if ("AMAZON.NoIntent" === intentName) {
-        handleAnswerRequest(intent, session, callback);
-    } else if ("AMAZON.StartOverIntent" === intentName) {
-        getWelcomeResponse(callback);
-    } else if ("AMAZON.RepeatIntent" === intentName) {
-        handleRepeatRequest(intent, session, callback);
-    } else if ("AMAZON.HelpIntent" === intentName) {
-        handleGetHelpRequest(intent, session, callback);
-    } else if ("AMAZON.StopIntent" === intentName) {
-        handleFinishSessionRequest(intent, session, callback);
-    } else if ("AMAZON.CancelIntent" === intentName) {
-        handleFinishSessionRequest(intent, session, callback);
-    } else {
-        throw "Invalid intent";
-    }*/
 }
 
 /**
@@ -171,137 +151,89 @@ var ANSWER_COUNT = 4;
 var GAME_LENGTH = 5;
 var CARD_TITLE = "Stopwatch"; // Be sure to change this for your skill.
 
-function getWelcomeResponse(callback) {
+function getWelcomeResponse(session, callback) {
     startTime = new Date();
-    var sessionAttributes = {},
-        speechOutput = `Stopwatch started at ${startTime.toString()}`,
-        shouldEndSession = false;
-        
-    sessionAttributes = {
-        "speechOutput": speechOutput,
-        "repromptText": speechOutput,
-        "startTime": startTime.getMilliseconds()
-    };
-    callback(sessionAttributes,
-        buildSpeechletResponse(CARD_TITLE, speechOutput, speechOutput, shouldEndSession));
+
+    console.log(`session ${session.user.userId}`);
+    dynamodb.putItem({
+                TableName: 'Stopwatches',
+                Item: {
+                    CustomerId: {
+                        S: session.user.userId
+                    },
+                    Data: {
+                        S: `${startTime.getTime()}`
+                    }
+                }
+            }, function (err, data) {
+                var sessionAttributes = {},
+                    speechOutput = `Quit interrupting the movie Eric. Stopwatch started at ${startTime.toString()}`,
+                    shouldEndSession = false;
+                
+                if (err) {
+                    console.log(err, err.stack);
+                    speechOutput = 'There was some error. Check the logs for more info';
+                }
+
+                sessionAttributes = {
+                    "speechOutput": speechOutput,
+                    "repromptText": speechOutput,
+                    "startTime": startTime
+                };
+                callback(sessionAttributes,
+                    buildSpeechletResponse(CARD_TITLE, speechOutput, speechOutput, shouldEndSession));
+            });
+
 }
 
-function populateRoundAnswers(gameQuestionIndexes, correctAnswerIndex, correctAnswerTargetLocation) {
-    // Get the answers for a given question, and place the correct answer at the spot marked by the
-    // correctAnswerTargetLocation variable. Note that you can have as many answers as you want but
-    // only ANSWER_COUNT will be selected.
-    var answers = [],
-        answersCopy = questions[gameQuestionIndexes[correctAnswerIndex]][Object.keys(questions[gameQuestionIndexes[correctAnswerIndex]])[0]],
-        temp, i;
+/**
+ * Handles the request to check time on a stopwatch
+ * @param {session} session
+ * @param {function} callback A function to call 
+ */
+function handleCheckRequest(session, callback) {
+    dynamodb.getItem({
+                TableName: 'Stopwatches',
+                Key: {
+                    CustomerId: {
+                        S: session.user.userId
+                    }
+                }
+            }, function (err, data) {
+                if (err) {
+                    console.log(err, err.stack);
+                    var speechOutput = 'An error has occurred, check the logs for more info';
+                    callback(session.attributes,
+                        buildSpeechletResponse(CARD_TITLE, speechOutput, speechOutput, true));
+                    return;
+                } else if (data.Item === undefined) {
+                    var speechOutput = 'There was no stopwatch started.';
+                    callback(session.attributes,
+                        buildSpeechletResponse(CARD_TITLE, speechOutput, speechOutput, true));
+                    return;
+                }
 
-    var index = answersCopy.length;
+                var foundStartTime = data.Item.Data.S;
 
-    if (index < ANSWER_COUNT){
-        throw "Not enough answers for question.";
-    }
-
-    // Shuffle the answers, excluding the first element.
-    for (var j = 1; j < answersCopy.length; j++){
-        var rand = Math.floor(Math.random() * (index - 1)) + 1;
-        index -= 1;
-
-        var temp = answersCopy[index];
-        answersCopy[index] = answersCopy[rand];
-        answersCopy[rand] = temp;
-    }
-
-    // Swap the correct answer into the target location
-    for (i = 0; i < ANSWER_COUNT; i++) {
-        answers[i] = answersCopy[i];
-    }
-    temp = answers[0];
-    answers[0] = answers[correctAnswerTargetLocation];
-    answers[correctAnswerTargetLocation] = temp;
-    return answers;
-}
-
-function handleAnswerRequest(intent, session, callback) {
-    var speechOutput = "";
-    var sessionAttributes = {};
-    var gameInProgress = session.attributes && session.attributes.questions;
-    var answerSlotValid = isAnswerSlotValid(intent);
-    var userGaveUp = intent.name === "DontKnowIntent";
-
-    if (!gameInProgress) {
-        // If the user responded with an answer but there is no game in progress, ask the user
-        // if they want to start a new game. Set a flag to track that we've prompted the user.
-        sessionAttributes.userPromptedToContinue = true;
-        speechOutput = "There is no game in progress. Do you want to start a new game? ";
-        callback(sessionAttributes,
-            buildSpeechletResponse(CARD_TITLE, speechOutput, speechOutput, false));
-    } else if (!answerSlotValid && !userGaveUp) {
-        // If the user provided answer isn't a number > 0 and < ANSWER_COUNT,
-        // return an error message to the user. Remember to guide the user into providing correct values.
-        var reprompt = session.attributes.speechOutput;
-        var speechOutput = "Your answer must be a number between 1 and " + ANSWER_COUNT + ". " + reprompt;
-        callback(session.attributes,
-            buildSpeechletResponse(CARD_TITLE, speechOutput, reprompt, false));
-    } else {
-        var gameQuestions = session.attributes.questions,
-            correctAnswerIndex = parseInt(session.attributes.correctAnswerIndex),
-            currentScore = parseInt(session.attributes.score),
-            currentQuestionIndex = parseInt(session.attributes.currentQuestionIndex),
-            correctAnswerText = session.attributes.correctAnswerText;
-
-        var speechOutputAnalysis = "";
-
-        if (answerSlotValid && parseInt(intent.slots.Answer.value) == correctAnswerIndex) {
-            currentScore++;
-            speechOutputAnalysis = "correct. ";
-        } else {
-            if (!userGaveUp) {
-                speechOutputAnalysis = "wrong. "
-            }
-            speechOutputAnalysis += "The correct answer is " + correctAnswerIndex + ": " + correctAnswerText + ". ";
-        }
-        // if currentQuestionIndex is 4, we've reached 5 questions (zero-indexed) and can exit the game session
-        if (currentQuestionIndex == GAME_LENGTH - 1) {
-            speechOutput = userGaveUp ? "" : "That answer is ";
-            speechOutput += speechOutputAnalysis + "You got " + currentScore.toString() + " out of "
-                + GAME_LENGTH.toString() + " questions correct. Thank you for playing!";
-            callback(session.attributes,
-                buildSpeechletResponse(CARD_TITLE, speechOutput, "", true));
-        } else {
-            currentQuestionIndex += 1;
-            var spokenQuestion = Object.keys(questions[gameQuestions[currentQuestionIndex]])[0];
-            // Generate a random index for the correct answer, from 0 to 3
-            correctAnswerIndex = Math.floor(Math.random() * (ANSWER_COUNT));
-            var roundAnswers = populateRoundAnswers(gameQuestions, currentQuestionIndex, correctAnswerIndex),
-
-                questionIndexForSpeech = currentQuestionIndex + 1,
-                repromptText = "Question " + questionIndexForSpeech.toString() + ". " + spokenQuestion + " ";
-            for (var i = 0; i < ANSWER_COUNT; i++) {
-                repromptText += (i+1).toString() + ". " + roundAnswers[i] + ". "
-            }
-            speechOutput += userGaveUp ? "" : "That answer is ";
-            speechOutput += speechOutputAnalysis + "Your score is " + currentScore.toString() + ". " + repromptText;
-
-            sessionAttributes = {
-                "speechOutput": repromptText,
-                "repromptText": repromptText,
-                "currentQuestionIndex": currentQuestionIndex,
-                "correctAnswerIndex": correctAnswerIndex + 1,
-                "questions": gameQuestions,
-                "score": currentScore,
-                "correctAnswerText":
-                    questions[gameQuestions[currentQuestionIndex]][Object.keys(questions[gameQuestions[currentQuestionIndex]])[0]][0]
-            };
-            callback(sessionAttributes,
-                buildSpeechletResponse(CARD_TITLE, speechOutput, repromptText, false));
-        }
-    }
+                var sessionAttributes = {},
+                    speechOutput = `Most recent stopwatch started ${(Date.now() - foundStartTime) / 60000} minutes ago`,
+                    shouldEndSession = false;
+                
+                sessionAttributes = {
+                    "speechOutput": speechOutput,
+                    "repromptText": speechOutput,
+                    "startTime": foundStartTime
+                };
+                callback(sessionAttributes,
+                    buildSpeechletResponse(CARD_TITLE, speechOutput, speechOutput, shouldEndSession));
+            });
 }
 
 function handleRepeatRequest(intent, session, callback) {
     // Repeat the previous speechOutput and repromptText from the session attributes if available
     // else start a new game session
     if (!session.attributes || !session.attributes.speechOutput) {
-        getWelcomeResponse(callback);
+        getWelcomeResponse(session, callback);
     } else {
         callback(session.attributes,
             buildSpeechletResponseWithoutCard(session.attributes.speechOutput, session.attributes.repromptText, false));
@@ -323,7 +255,8 @@ function handleGetHelpRequest(intent, session, callback) {
         + "Would you like to keep playing?",
         repromptText = "To give an answer to a question, respond with the number of the answer . "
         + "Would you like to keep playing?";
-        var shouldEndSession = false;
+
+    var shouldEndSession = false;
     callback(session.attributes,
         buildSpeechletResponseWithoutCard(speechOutput, repromptText, shouldEndSession));
 }
@@ -360,7 +293,7 @@ function buildSpeechletResponse(title, output, repromptText, shouldEndSession) {
                 text: repromptText
             }
         },
-        shouldEndSession: shouldEndSession
+        shouldEndSession: true //shouldEndSession
     };
 }
 
@@ -376,7 +309,7 @@ function buildSpeechletResponseWithoutCard(output, repromptText, shouldEndSessio
                 text: repromptText
             }
         },
-        shouldEndSession: shouldEndSession
+        shouldEndSession: true//shouldEndSession
     };
 }
 
